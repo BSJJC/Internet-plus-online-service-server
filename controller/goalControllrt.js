81676efa1e94b4bda2a166cda156c05af6e9d119
BSJJC <805086021@qq.com>
2023-03-08 20:15:00 +0800

    feat: express async handler

--- controller/goalControllrt.js
+++ controller/goalControllrt.js
@@ -1,38 +1,44 @@
+const asyncHandler = require("express-async-handler");
+
 /**
  * @description   Get goals
  * @route              GET /api/goals
  * @access           Privite
  */
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: "Get goals" });
-};
+});
 
 /**
  * @description   Set goals
  * @route              POST /api/goals
  * @access           Privite
  */
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
+  if (!req.body.msg) {
+    throw new Error("no value");
+  }
+
   res.status(200).json({ msg: "Set goal" });
-};
+});
 
 /**
  * @description   Update goals
  * @route              PUT /api/goals/:id
  * @access           Privite
  */
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: `Update goal ${req.params.id}` });
-};
+});
 
 /**
  * @description   Delete goals
  * @route              DELETE /api/goals/:id
  * @access           Privite
  */
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
